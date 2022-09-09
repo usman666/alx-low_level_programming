9331472f604459527f7ce7ea3621df2d6a9f5a54
usman <babayi250.au37@gmail.com>
2022-09-09 16:26:00 -0700

    task 12;

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,22 +18,22 @@ int main(void)
 	int com = 44;
 	while (a <= 99)
 	{
-		b = a + 1;
-		while (b <= 99)
-		{
-			putchar((a / 10) + n1);
-			putchar((a % 10) + n1);
-			putchar(32);
-			putchar((b / 10) + n1);
-			putchar((b % 10) + n1);
-			if (a != 98 || b != 99)
-			{
-				putchar(com);
-				putchar(32);
-			}
-			b += 1;
-		}
-		a += 1;
+	  b = a + 1;
+	 while (b <= 99)
+	{
+	putchar((a / 10) + n1);
+	putchar((a % 10) + n1);
+	putchar(32);
+	putchar((b / 10) + n1);
+	putchar((b % 10) + n1);
+	if (a != 98 || b != 99)
+	  {
+	 putchar(com);
+	 putchar(32);
+	  }
+	b += 1;
+	}
+	a += 1;
 	}
 	putchar('\n');
 	return (0);
