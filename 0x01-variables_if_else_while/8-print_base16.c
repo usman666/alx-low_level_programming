09e941421cec3ce79deeccaafe62b18d077768dc
usman666 <55113190+usman666@users.noreply.github.com>
2022-09-11 23:37:11 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,10 +3,9 @@
 #include <time.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
