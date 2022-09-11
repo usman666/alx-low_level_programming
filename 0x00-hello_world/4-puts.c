13d22ae21cb184a1a859b1ce9c966fce374bf310
usman666 <babayi250.au37@gmail.com>
2022-09-11 14:26:06 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 int main(void)
 {
+	/**
+	 * This serve as the entry point
+	 * Return: Always returns 0
+	 */
+
 	puts("\"programming is like building a multilingual puzzle");
 
 	return (0);
