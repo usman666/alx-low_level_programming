63d31ab8eb42d81103a28da7148ce8d9984131f4
usman666 <babayi250.au37@gmail.com>
2022-09-15 13:35:23 -0700

    task10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*print_triangle - prints a triangle
+*@size:size parameter of triangle
+*Return: returns nothing
+*/
+
+void print_triangle(int size)
+{
+	int x, y;
+
+	if (size > 0)
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for ((y = size - x); y > 0; y--)
+			{
+				putchar(' ');
+			}
+
+			for (y = 0; y< x; y++)
+			{
+				putchar('#');
+			}
+
+			if (x == size)
+			{
+				continue;
+			}
+
+			putchar('\n');
+		}
+	}
+	putchar('\n');
+}
