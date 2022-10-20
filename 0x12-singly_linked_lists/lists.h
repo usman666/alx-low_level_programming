62ae6f66897e7e34fa4a66732739781e385b0ed5
usman <babayi250.au37@gmail.com>
2022-10-20 15:39:39 -0700

    lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,3 @@
-
-
 #ifndef LISTS_H
 #define LISTS_H
 
@@ -34,4 +32,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif 
+#endif
